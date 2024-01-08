e7c06576847c73d2cecdb6c2f81d23f0487fda21
Henrique Broering Rohden <hrohden@gmail.com>
2024-01-08 15:22:59 -0300

    Add initial tasks to getAllTasks method

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,11 +1,28 @@
 import { Controller, Get } from '@nestjs/common';
+import { Task } from './types/Task';
+import { statusList } from './types/status';
 
 @Controller('tasks')
 export class TasksController {
   constructor() {}
 
+  private tasks: Task[] = [
+    {
+      id: '1',
+      status: statusList[1],
+      title: 'Create Taskify application',
+      description: 'Initial code to showcase some features.',
+    },
+    {
+      id: '2',
+      status: statusList[1],
+      title: 'Create GitHub repository',
+      description: 'Create repo and push code to it',
+    },
+  ];
+
   @Get()
-  getAllTasks() {
-    return 'Get all tasks';
+  getAllTasks(): Task[] {
+    return this.tasks;
   }
 }
